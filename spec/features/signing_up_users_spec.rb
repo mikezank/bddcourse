dfc3ba9e076d8013c8fd78c1d266bd64210830b6
mike zanchettin <mike.zanchettin@gmail.com>
2018-08-04 15:45:59 -0500

    Set up user sign-in

--- spec/features/signing_up_users_spec.rb
+++ spec/features/signing_up_users_spec.rb
@@ -6,8 +6,8 @@ RSpec.feature "Sign Up Users" do
     visit "/"
     click_link "Sign up"
     fill_in "Email", with: "user@example.com"
-    fill_in "Password", with: "pass"
-    fill_in "Password confirmation", with: "pass"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
     click_button "Sign up"
 
     expect(page).to have_content("You have signed up successfully")
